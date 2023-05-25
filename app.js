1d4c60854144afc6d71863f95993d8fcd6fa07b8
peteryen15 <peteryen15@gmail.com>
2023-05-25 15:41:42 +0800

    try "delete data"

--- app.js
+++ app.js
@@ -108,6 +108,19 @@ app.put("/students/edit/:id", async (req, res) => {
   }
 });
 
+app.delete("/students/delete/:id", (req, res) => {
+  let { id } = req.params;
+  Student.deleteOne({ id })
+    .then((meg) => {
+      console.log(meg);
+      res.send("Deleted successfully.");
+    })
+    .catch((e) => {
+      console.log(e);
+      res.send("Deleted failed");
+    });
+});
+
 app.get("/*", (req, res) => {
   res.status(404);
   res.send("Not allowed.");
